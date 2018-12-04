f1f20175434d5fe2e6e65e74caaa55a1af081e88
Valerii Bilenko <valerii.bilenko@wearebrain.com>
2018-12-04 12:28:17 +0200

    DH parameters bug fixed + angle normalization added

--- src/main/java/com/robotcontrol/tests/MainTest.java
+++ src/main/java/com/robotcontrol/tests/MainTest.java
@@ -1,28 +1,54 @@
 package com.robotcontrol.tests;
 
-import com.robotcontrol.calc.positionalControl.controllers.PositionalController;
-import com.robotcontrol.calc.positionalControl.entities.PositionalPath;
-import com.robotcontrol.calc.stepperControl.controllers.PathConverter;
-import com.robotcontrol.calc.stepperControl.entities.SteppersPath;
-import com.robotcontrol.calc.stepperControl.entities.SteppersPoint;
+import com.robotcontrol.calc.DHParameters.SCARADH;
 import com.robotcontrol.exc.BoundsViolation;
 
-import java.util.List;
-
-import static com.robotcontrol.parameters.dynamic.Position.CURRENT_POSITION;
+import java.util.Arrays;
 
 public class MainTest {
     public static void main(String[] args) throws BoundsViolation {
-        PositionalPath positionalPath = PositionalController.moveToPointPos(CURRENT_POSITION, new double[]{10,10,12});
-        SteppersPath steppersPath = PathConverter.convertToSteppersPath(positionalPath);
-        List<SteppersPoint> steppersPoints = steppersPath.getSteppersPoints();
+//        double[] initial = {0, 0, Math.random() * 10};
+//
+//        for (int i = 0; i < 40; i++) {
+////            double[] initial = {0, Math.PI/2, Math.random() * 10};
+//            initial[0] += 0.1;
+//            initial[1] += 0.1;
+//
+//            double[] forwardKinematics = SCARADH.forwardKinematics(initial);
+//            double[] inverseKinematics = SCARADH.inverseKinematics(forwardKinematics);
+//
+//            System.out.println("initial=" + Arrays.toString(initial));
+//            System.out.println("inverse=" + Arrays.toString(inverseKinematics));
+//            System.out.println("forward=" + Arrays.toString(forwardKinematics));
+//            double[] forward = SCARADH.forwardKinematics(inverseKinematics);
+//            System.out.println("result =" + Arrays.toString(forward));
+//            System.out.println();
+//        }
 
-        long total = 0;
-        for (int i = 0; i < steppersPoints.size(); i++) {
-            total += steppersPoints.get(i).getStepsDelays()[2];
-            System.out.println(steppersPoints.get(i).getStepsDelays()[2]);
+        for (int i = 0; i < 50; i++) {
+            double[] initCoords = new double[] {Math.random()*100,Math.random()*100,Math.random()*100};
+
+            double[] resultAngl = SCARADH.inverseKinematics(initCoords);
+            double[] resultCoords = SCARADH.forwardKinematics(resultAngl);
+            double[] secondAngl = SCARADH.inverseKinematics(resultCoords);
+
+            System.out.println("init coords   =" + Arrays.toString(initCoords));
+            System.out.println("result coords =" + Arrays.toString(resultCoords));
+            System.out.println("result angles =" + Arrays.toString(resultAngl));
+            System.out.println("second angles =" + Arrays.toString(secondAngl));
+            System.out.println();
         }
 
-        System.out.println(total);
+//        PositionalPath positionalPath = PositionalController.moveToPointPos(CURRENT_POSITION, new double[]{10,10,12});
+//        SteppersPath steppersPath = PathConverter.convertToSteppersPath(positionalPath);
+//        List<SteppersPoint> steppersPoints = steppersPath.getSteppersPoints();
+//
+//        long total = 0;
+//        for (int i = 0; i < steppersPoints.size(); i++) {
+//            total += steppersPoints.get(i).getStepsDelays()[2];
+//            System.out.println(steppersPoints.get(i).getStepsDelays()[2]);
+//        }
+//
+//        System.out.println(total);
     }
 }
